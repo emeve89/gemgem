4692e1cb269bf6df2041cd2249308c6d474e43b6
Mario Viapiano <marioeviapiano@gmail.com>
2016-05-05 12:50:14 -0300

    Add validation specs to Thing::Create

--- spec/concepts/thing/crud_spec.rb
+++ spec/concepts/thing/crud_spec.rb
@@ -11,4 +11,19 @@ describe Thing::Create do
     expect(thing.name).to eq 'Rails'
     expect(thing.description).to eq 'Kickass web dev'
   end
+
+  it 'invalid' do
+    res, op = Thing::Create.run(thing: {name: ''})
+
+    expect(res).to eq false
+    expect(op.model).to_not be_persisted
+    expect(op.contract.errors.to_s).to eq "{:name=>[\"can't be blank\"]}"
+  end
+
+  it 'invalid description' do
+    res, op = Thing::Create.run(thing: {name: 'Rails', description: 'hi'})
+
+    expect(res).to eq false
+    expect(op.contract.errors.to_s).to eq "{:description=>[\"is too short (minimum is 4 characters)\"]}"
+  end
 end
