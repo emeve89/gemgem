ba6d55d7f670c70c3902cf845b140c5739fd14b0
Mario Viapiano <marioeviapiano@gmail.com>
2016-05-05 13:24:27 -0300

    Add specs to Thing::Update

--- spec/concepts/thing/crud_spec.rb
+++ spec/concepts/thing/crud_spec.rb
@@ -1,29 +1,49 @@
 require 'rails_helper'
 
-describe Thing::Create do
-  it 'persists valid' do
-    thing = Thing::Create.(thing: {
+describe 'Thing CRUD' do
+  let(:thing) do
+    Thing::Create.(thing: {
       name: 'Rails',
       description: 'Kickass web dev'
     }).model
-
-    expect(thing).to be_persisted
-    expect(thing.name).to eq 'Rails'
-    expect(thing.description).to eq 'Kickass web dev'
   end
 
-  it 'invalid' do
-    res, op = Thing::Create.run(thing: {name: ''})
+  describe 'Create' do
+    it 'persists valid' do
+      expect(thing).to be_persisted
+      expect(thing.name).to eq 'Rails'
+      expect(thing.description).to eq 'Kickass web dev'
+    end
 
-    expect(res).to eq false
-    expect(op.model).to_not be_persisted
-    expect(op.contract.errors.to_s).to eq "{:name=>[\"can't be blank\"]}"
+    it 'invalid' do
+      res, op = Thing::Create.run(thing: {name: ''})
+
+      expect(res).to eq false
+      expect(op.model).to_not be_persisted
+      expect(op.contract.errors.to_s).to eq "{:name=>[\"can't be blank\"]}"
+    end
+
+    it 'invalid description' do
+      res, op = Thing::Create.run(thing: {name: 'Rails', description: 'hi'})
+
+      expect(res).to eq false
+      expect(op.contract.errors.to_s).to eq "{:description=>[\"is too short (minimum is 4 characters)\"]}"
+    end
   end
 
-  it 'invalid description' do
-    res, op = Thing::Create.run(thing: {name: 'Rails', description: 'hi'})
+  describe 'Update' do
+    it 'persists valid, ignores name' do
+      Thing::Update.(
+        id: thing.id,
+        thing: {
+          name: 'Lotus',
+          description: 'Simply better..'
+        }
+      )
 
-    expect(res).to eq false
-    expect(op.contract.errors.to_s).to eq "{:description=>[\"is too short (minimum is 4 characters)\"]}"
+      thing.reload
+      expect(thing.name).to eq 'Rails'
+      expect(thing.description).to eq 'Simply better..'
+    end
   end
 end
